02a7620a5b31c298d024511c33e45ac811d5ecd1
spadarynjaALINA <alinagusarik@gmail.com>
2022-07-18 15:09:24 +0300

    fix:fix errors

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-const {Transform} = require('stream') ;
-const {stdin} = require('process')
-const {stdout} = require('process')
-
-  const transform = async () =>
- {
-    class TransformStream extends Transform
-    {
-        constructor ( options = {} )
-        {
-        super(options)
-    }
-
-  _transform(chunk, enc, callback) {
-          const chunkStringField = chunk.toString();
-          const reverseChunk =`${chunkStringField.split('').reverse().join('')}\n`
-    this.push(reverseChunk)
-
-      callback();
-    }
-  }
-    const transformString = new TransformStream()
-    stdin.pipe(transformString).pipe(stdout)
-
-};
-transform()
+process.stdin.on('data',(string)=>{
+process.stdout.write(string.toString().split('').reverse().join(''))
+})
